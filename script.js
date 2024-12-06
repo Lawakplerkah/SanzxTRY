324552d4877ee04151c9bedf3b24b322fd337446
Reza <143893544+Lawakplerkah@users.noreply.github.com>
2024-12-06 18:17:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Daftar produk
 const produk = [
   { nama: "Paket Sewa Bot", harga: "Rp15.000 - Rp50.000", gambar: "https://files.catbox.moe/7prydh.jpg" },
-  { nama: "Paket Premium", harga: "Rp5.000 - Rp30.000", gambar: "https://files.catbox.moe/7prydh.jpg" }
+  { nama: "Paket Premium", harga: "Rp5.000 - Rp30.000", gambar: "https://files.catbox.moe/7prydh.jpg" },
+  { nama: "Paket Custom", harga: "Hubungi Admin", gambar: "https://files.catbox.moe/7prydh.jpg" }
 ];
 
 // Render daftar produk
@@ -33,10 +34,10 @@ document.getElementById("btn-sewa").onclick = () => {
   modalText.innerHTML = `<pre>
 *LIST HARGA SEWA BOT*
 
-*PAKET S1*
-- Rp15.000 / Group
-...
-Berminat? Hubungi: <a href="https://wa.me/6283894064758" target="_blank">WhatsApp</a>
+*PAKET S1*: Rp15.000 / Group
+*PAKET S2*: Rp25.000 / Group
+*PAKET S3*: Rp40.000 / Group
+Berminat? <a href="https://wa.me/6283894064758" target="_blank">Hubungi Admin</a>
 </pre>`;
 };
 
@@ -45,9 +46,9 @@ document.getElementById("btn-premium").onclick = () => {
   modalText.innerHTML = `<pre>
 *LIST HARGA PREMIUM*
 
-*PAKET P1*
-- Rp5.000 / 7 Day
-...
-Berminat? Hubungi: <a href="https://wa.me/6283894064758" target="_blank">WhatsApp</a>
+*PAKET P1*: Rp5.000 / 7 Hari
+*PAKET P2*: Rp10.000 / 15 Hari
+*PAKET P3*: Rp20.000 / 30 Hari
+Berminat? <a href="https://wa.me/6283894064758" target="_blank">Hubungi Admin</a>
 </pre>`;
 };
